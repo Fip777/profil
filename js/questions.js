384426ffa41a63e81f1c211ad67349d604297784
Fip777 <first.prod1000@hotmail.com>
2026-06-11 15:46:24 +0200

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,109 +1,138 @@
+/* =============
+   COPYRIGHT
+================
+Copyright (C) 2026, Audrey Bourgeois. Tous droits réservés. 
+Les informations contenues ici, code et concepts intellectuels, sont la propriété de Audrey Bourgeois 
+et sont protégés par le droit d'auteur. 
+Toute diffusion ou reproduction même partielle, quel qu’en soit le support, est interdite.*/
+
 const questions = [
-  {
-    text: "Question 1 ?",
+   {
+    text: "Question 1:",
     answers: [
-      { label: "Réponse 1-1", value: "A" },
-      { label: "Réponse 1-2", value: "B" }
-    ]
-  },
-  {
-    text: "Question 2 ?",
-    answers: [
-      { label: "Réponse 2-1", value: "A" },
-      { label: "Réponse 2-2", value: "B" }
+      { label: "Réponse A", value: "A" },
+      { label: "Réponse B", value: "B" }
     ]
   },
   {
-    text: "Question 3 ?",
+    text: "Question 2:",
     answers: [
-      { label: "Réponse 3-1", value: "A" },
-      { label: "Réponse 3-2", value: "B" }
+      { label: "Réponse A", value: "A" },
+      { label: "Réponse B", value: "B" }
+    ]
+  },
+  {
+    text: "Question 3:",
+    answers: [
+      { label: "Réponse A", value: "A" },
+      { label: "Réponse B", value: "B" }
     ]
   },
    {
-    text: "Question 4 ?",
+    text: "Question 4:",
     answers: [
-      { label: "Réponse 4-1", value: "A" },
-      { label: "Réponse 4-2", value: "B" }
+      { label: "Réponse A", value: "A" },
+      { label: "Réponse B", value: "B" }
     ]
   },
    {
-    text: "Question 5 ?",
+    text: "Question 5:",
     answers: [
-      { label: "Réponse 5-1", value: "A" },
-      { label: "Réponse 5-2", value: "B" }
+      { label: "Réponse A", value: "A" },
+      { label: "Réponse B", value: "B" }
     ]
   },
    {
-    text: "Question 6 ?",
+    text: "Question 6:",
     answers: [
-      { label: "Réponse 6-1", value: "A" },
-      { label: "Réponse 6-2", value: "B" }
+      { label: "Réponse A", value: "A" },
+      { label: "Réponse B", value: "B" }
     ]
   },
    {
-    text: "Question 7 ?",
+    text: "Question 7:",
     answers: [
-      { label: "Réponse 7-1", value: "A" },
-      { label: "Réponse 7-2", value: "B" }
+      { label: "Réponse A", value: "A" },
+      { label: "Réponse B", value: "B" }
     ]
   },
    {
-    text: "Question 8 ?",
+    text: "Question 8:",
     answers: [
-      { label: "Réponse 8-1", value: "A" },
-      { label: "Réponse 8-2", value: "B" }
+      { label: "Réponse A", value: "A" },
+      { label: "Réponse B", value: "B" }
     ]
   },
    {
-    text: "Question 9 ?",
+    text: "Question 9:",
     answers: [
-      { label: "Réponse 9-1", value: "A" },
-      { label: "Réponse 9-2", value: "B" }
+      { label: "Réponse A", value: "A" },
+      { label: "Réponse B", value: "B" }
     ]
   },
    {
-    text: "Question 10 ?",
+    text: "Question 10:",
     answers: [
-      { label: "Réponse 10-1", value: "A" },
-      { label: "Réponse 10-2", value: "B" }
+      { label: "Réponse A", value: "A" },
+      { label: "Réponse B", value: "B" }
+    ]
+  },   
+   {
+    text: "Question 11:",
+    answers: [
+      { label: "Réponse A", value: "A" },
+      { label: "Réponse B", value: "B" }
     ]
   },
    {
-    text: "Question 11 ?",
+    text: "Question 12:",
     answers: [
-      { label: "Réponse 11-1", value: "A" },
-      { label: "Réponse 11-2", value: "B" }
+      { label: "Réponse A", value: "A" },
+      { label: "Réponse B", value: "B" }
     ]
   },
    {
-    text: "Question 12 ?",
+    text: "Question 13:",
     answers: [
-      { label: "Réponse 12-1", value: "A" },
-      { label: "Réponse 12-2", value: "B" }
+      { label: "Réponse A", value: "A" },
+      { label: "Réponse B", value: "B" }
     ]
   },
    {
-    text: "Question 13 ?",
+    text: "Question 14:",
     answers: [
-      { label: "Réponse 13-1", value: "A" },
-      { label: "Réponse 13-2", value: "B" }
+      { label: "Réponse A", value: "A" },
+      { label: "Réponse B", value: "B" }
     ]
   },
    {
-    text: "Question 14 ?",
+    text: "Question 15:",
     answers: [
-      { label: "Réponse 14-1", value: "A" },
-      { label: "Réponse 14-2", value: "B" }
+      { label: "Réponse A", value: "A" },
+      { label: "Réponse B", value: "B" }
     ]
   },
    {
-    text: "Question 15 ?",
+    text: "Question 16:",
     answers: [
-      { label: "Réponse 15-1", value: "A" },
-      { label: "Réponse 15-2", value: "B" }
+      { label: "Réponse A", value: "A" },
+      { label: "Réponse B", value: "B" }
     ]
-  }
+  },
+   {
+    text: "Question 17:",
+    answers: [
+      { label: "Réponse A", value: "A" },
+      { label: "Réponse B", value: "B" }
+    ]
+  },
+  {
+    text: "Question 18:",
+    answers: [
+      { label: "Réponse A", value: "A" },
+      { label: "Réponse B", value: "B" }
+    ]
+  },
 ]
 /* ================================
    GESTION PAGE D’ACCUEIL & MODALE
